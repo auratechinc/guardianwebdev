f9b4e407b117aab175e1bbb2fce5661385e0048d
PixchureLive <74546734+Pixchure@users.noreply.github.com>
2021-06-15 09:25:07 -0400

    Update face-detection.js

--- js/face-detection.js
+++ js/face-detection.js
@@ -41,15 +41,15 @@ $("#detection-switch").change(function () {
   if(this.checked){
     toggleContrl("box-switch", true);
     //toggleContrl("landmarks-switch", true);
-    toggleContrl("expression-switch", true);
-    toggleContrl("age-gender-switch", true);
+    //toggleContrl("expression-switch", true);
+    //toggleContrl("age-gender-switch", true);
     $("#box-switch").prop('checked', true);
     $(".loading").removeClass('d-none');
     Promise.all([
       faceapi.nets.tinyFaceDetector.load(modelPath),
       //faceapi.nets.faceLandmark68TinyNet.load(modelPath),
-      faceapi.nets.faceExpressionNet.load(modelPath),
-      faceapi.nets.ageGenderNet.load(modelPath)
+      //faceapi.nets.faceExpressionNet.load(modelPath),
+      //faceapi.nets.ageGenderNet.load(modelPath)
     ]).then(function(){
       createCanvas();
       startDetection();
@@ -58,9 +58,9 @@ $("#detection-switch").change(function () {
   else {
     clearInterval(faceDetection);
     toggleContrl("box-switch", false);
-    toggleContrl("landmarks-switch", false);
-    toggleContrl("expression-switch", false);
-    toggleContrl("age-gender-switch", false);
+    //toggleContrl("landmarks-switch", false);
+    //toggleContrl("expression-switch", false);
+    //toggleContrl("age-gender-switch", false);
     if(typeof canvas !== "undefined"){
       setTimeout(function() {
         canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
